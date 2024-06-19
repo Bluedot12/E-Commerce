7ece103f95b49ad9b83264c7b149c4717998a390
Bluedot12 <mounica.ra@gmail.com>
2024-06-19 17:06:41 +0530

    product update

--- src/app/my-service.service.ts
+++ src/app/my-service.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 export interface product {
+  img:null,
   id: number,
   title: string,
   price: string,
--- src/app/product/main/main.component.ts
+++ src/app/product/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MyServiceService, product } from 'src/app/my-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class MainComponent {
 
+  prod: product[] =[];
+
+  constructor(private dataService:MyServiceService,private router:Router){}
+
+  ngOnInit(){
+    this.loadData();
+  }
+
+  loadData(){
+    this.dataService.getData().subscribe((data)=>{
+      this.prod=data;
+      console.log(this.prod);
+      
+    })
+  }
+
+  navigateToDetail(){
+    this.router.navigate([])
+  }
+
+  navigateToCart(){
+    this.router.navigate([])
+  }
 }
